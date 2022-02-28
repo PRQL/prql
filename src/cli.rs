65893803e0d3dd167a387ea58f555610091a8ada
Kyle Wigley <kwigley44@gmail.com>
2022-02-28 15:06:28 -0500

    expose prql as lib + integration test (#142)

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,12 @@
-use std::io::{Read, Write};
-
+use crate::parser::{ast_of_string, Rule};
 use anyhow::Error;
 use clap::{ArgEnum, Parser};
 use clio::{Input, Output};
+use std::io::{Read, Write};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
 enum Dialect {
-    Sql,
+    Ast,
 }
 
 #[derive(Parser)]
@@ -18,7 +18,7 @@ pub struct Cli {
     #[clap(short, long, default_value = "-", parse(try_from_os_str = Output::try_from))]
     output: Output,
 
-    #[clap(short, long, arg_enum, default_value = "sql")]
+    #[clap(short, long, arg_enum, default_value = "ast")]
     format: Dialect,
 }
 
@@ -27,11 +27,9 @@ impl Cli {
         let mut source = String::new();
         self.input.read_to_string(&mut source)?;
         match self.format {
-            Dialect::Sql => writeln!(
-                self.output,
-                "TODO! do something useful with source:\n{}",
-                &source
-            )?,
+            Dialect::Ast => self
+                .output
+                .write_all(&serde_yaml::to_vec(&ast_of_string(&source, Rule::query)?)?)?,
         };
         Ok(())
     }
