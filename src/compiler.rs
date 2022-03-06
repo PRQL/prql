deee88b2197ece41faa56280f4e2c7619512855e
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2022-03-06 15:10:45 -0800

    Put important functions at the top of files (#160)

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,6 +3,25 @@ use anyhow::{anyhow, Result};
 use itertools::Itertools;
 use std::{collections::HashMap, iter::zip};
 
+pub fn compile(ast: Item) -> Result<Item> {
+    let functions = load_std_lib()?;
+    let mut run_functions = RunFunctions::new();
+    functions.into_iter().for_each(|f| {
+        run_functions.add_function(&f.into_function().unwrap());
+    });
+    let mut replace_variables = ReplaceVariables::new();
+    // TODO: is it always OK to run these serially?
+    let ast = run_functions.fold_item(&ast)?;
+    let ast = replace_variables.fold_item(&ast)?;
+    Ok(ast)
+}
+
+fn load_std_lib() -> Result<Items> {
+    use super::parse;
+    let std_lib = include_str!("stdlib.prql");
+    Ok(parse(std_lib)?.into_inner_items())
+}
+
 // Fold pattern:
 // - https://rust-unofficial.github.io/patterns/patterns/creational/fold.html
 // Good discussions on the visitor / fold pattern:
@@ -340,25 +359,6 @@ impl Filter {
                 .collect(),
         )
     }
-}
-
-fn load_std_lib() -> Result<Items> {
-    use super::parse;
-    let std_lib = include_str!("stdlib.prql");
-    Ok(parse(std_lib)?.into_inner_items())
-}
-
-pub fn compile(ast: Item) -> Result<Item> {
-    let functions = load_std_lib()?;
-    let mut run_functions = RunFunctions::new();
-    functions.into_iter().for_each(|f| {
-        run_functions.add_function(&f.into_function().unwrap());
-    });
-    let mut replace_variables = ReplaceVariables::new();
-    // TODO: is it always OK to run these serially?
-    let ast = run_functions.fold_item(&ast)?;
-    let ast = replace_variables.fold_item(&ast)?;
-    Ok(ast)
 }
 
 #[cfg(test)]
