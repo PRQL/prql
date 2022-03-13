ec5f6f815bbaf4fbf41e862d623b1ddfa9391143
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2022-03-12 17:06:42 -0800

    Add CLI converting from PRQL to SQL (#179)

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,8 @@ pub use cli::Cli;
 pub use compiler::compile;
 pub use parser::parse;
 pub use to_sql::sql_of_ast;
+
+/// Convert PRQL string to SQL string.
+pub fn sql_of_prql(prql: &str) -> Result<String> {
+    parse(prql).and_then(compile).and_then(|x| sql_of_ast(&x))
+}
