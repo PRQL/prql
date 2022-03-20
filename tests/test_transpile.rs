94aefddf49b5d04170296598e7d8822130a070d7
Maximilian Roos <m@maxroos.com>
2022-03-20 11:48:08 -0700

    Consolidate test crates
    These are each a separate compilation unit, so when running something
    like `watchexec -- cargo test`, the extra latency matters. At the
    moment, there's no need to have more than one.

--- tests/test_transpile.rs
+++ tests/test_transpile.rs
@@ -1,6 +1,20 @@
 use insta::assert_snapshot;
-use prql::transpile;
-use prql::Result;
+use prql::*;
+
+#[test]
+fn parse_simple_string_to_ast() -> Result<()> {
+    assert_eq!(
+        serde_yaml::to_string(&parse("select 1")?)?,
+        r#"---
+Query:
+  items:
+    - Pipeline:
+        - Select:
+            - Raw: "1"
+"#
+    );
+    Ok(())
+}
 
 #[test]
 fn transpile_variables() -> Result<()> {
