f53888d7266e7cd379a0f4d53f18eda2fd69d05d
shota_kizawa <shomok0106k@gmail.com>
2022-03-21 13:03:07 +0900

    fix test

--- src/translator.rs
+++ src/translator.rs
@@ -784,44 +784,6 @@ Query:
         "###
         );
 
-        // table
-        let query = parse(
-            r#"
-        table newest_employees = (
-            from employees
-            sort tenure
-            take 50
-        )
-        table average_salaries = (
-            from salaries
-            aggregate by:country [
-                average_country_salary: average salary
-            ]
-        )
-        from newest_employees
-        join average_salaries [country]
-        select [name, salary, average_country_salary]
-    "#,
-        )?;
-        let ast = materialize(query)?;
-        let sql = translate(&ast)?;
-        assert_display_snapshot!(sql,
-            @r###"
-        WITH newest_employees AS (
-            SELECT TOP 50 * FROM employees
-            ORDER BY tenure
-        )
-        WITH average_salaries AS (
-            SELECT avg(salary) as average_country_salary
-            FROM salaries
-            GROUP BY country
-        )
-        SELECT name, salary
-        FROM newest_employees
-        JOIN average_salaries USING (country)
-        "###
-        );
-
         let query = parse(
             r#"
 from employees
@@ -849,6 +811,61 @@ take 20
         let ast = materialize(query)?;
         let sql = translate(&ast)?;
         assert_display_snapshot!(sql);
+
+        // table
+        let query = parse(
+            r#"
+        table newest_employees = (
+            from employees
+            sort tenure
+            take 50
+        )
+        table average_salaries = (
+            from salaries
+            aggregate by:country [
+                average_country_salary: average salary
+            ]
+        )
+        from newest_employees
+        join average_salaries [country]
+        select [name, salary, average_country_salary]
+    "#,
+        )?;
+        let ast = materialize(query)?;
+        let sql = translate(&ast)?;
+        assert_display_snapshot!(sql,
+            @r###"
+        WITH newest_employees AS (
+          SELECT
+            TOP (50) *
+          FROM
+            employees
+          ORDER BY
+            tenure
+        ),
+        average_salaries AS (
+          SELECT
+            AVG(salary) AS average_country_salary
+          FROM
+            salaries
+          GROUP BY
+            country
+        ),
+        table_0 AS (
+          SELECT
+            name,
+            salary,
+            average_country_salary
+          FROM
+            newest_employees
+            JOIN average_salaries ON country
+        )
+        SELECT
+          *
+        FROM
+          table_0
+        "###
+        );
 
         Ok(())
     }
