c08a9d099279fa1a76c20c1342efec4ef69e035d
shota_kizawa <shomok0106k@gmail.com>
2022-03-21 12:55:54 +0900

    fix: parse table

--- src/translator.rs
+++ src/translator.rs
@@ -784,6 +784,44 @@ Query:
         "###
         );
 
+        // table
+        let query = parse(
+            r#"
+        table newest_employees = (
+            from employees
+            sort tenure
+            take 50
+        )
+        table average_salaries = (
+            from salaries
+            aggregate by:country [
+                average_country_salary: average salary
+            ]
+        )
+        from newest_employees
+        join average_salaries [country]
+        select [name, salary, average_country_salary]
+    "#,
+        )?;
+        let ast = materialize(query)?;
+        let sql = translate(&ast)?;
+        assert_display_snapshot!(sql,
+            @r###"
+        WITH newest_employees AS (
+            SELECT TOP 50 * FROM employees
+            ORDER BY tenure
+        )
+        WITH average_salaries AS (
+            SELECT avg(salary) as average_country_salary
+            FROM salaries
+            GROUP BY country
+        )
+        SELECT name, salary
+        FROM newest_employees
+        JOIN average_salaries USING (country)
+        "###
+        );
+
         let query = parse(
             r#"
 from employees
