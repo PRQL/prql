2bd0b73256e96ba18590308911944b15edc68133
Maximilian Roos <m@maxroos.com>
2022-03-31 10:28:07 -0700

    Remove old comments
    @aljazerzen completed these in #287

--- prql/tests/integration/examples.rs
+++ prql/tests/integration/examples.rs
@@ -1,11 +1,3 @@
-// TODO:
-// - It would be a bit nicer to have the script be in rust; so it didn't
-//   depend on hoping we get a recently compiled version in `./target/debug` /
-//   could run on any platform.
-// - Currently it mixes "not yet implemented" with "an actual error". We should
-//   really disaggregate those; e.g. add a comment of "not yet implemented" for
-//   those that error, rather than just ignoring them.
-
 use insta::{assert_snapshot, glob};
 use std::fs;
 use std::path::Path;
