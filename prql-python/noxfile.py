6b145ca8b18fedad4203ad0f67e007ddf08c8946
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-04-24 20:10:34 +0100

    Support nox for testing multiple python

---
+++ prql-python/noxfile.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Nox session configuration."""
+import os
+from typing import Any, List
+from pathlib import Path
+
+import nox
+from nox.sessions import Session
+
+VERSIONS: List[str] = [
+    "3.6",
+    "3.7",
+    "3.8",
+    "3.9",
+    "3.10",
+]
+
+nox.options.stop_on_first_error = False
+nox.options.reuse_existing_virtualenvs = False
+
+
+@nox.session(python=VERSIONS)
+def tests(session: Session) -> None:
+    """Run the test suite with pytest."""
+    session.install("-v", f"--find-links={Path('..', 'dist')}", "prql_python")
+    session.install("-v", "-r", "requirements.txt")
+    session.run("pytest", str(Path("python", "tests")))
