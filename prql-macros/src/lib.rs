17c6d217704d9284b0634154a749679549e206e4
Aljaž Mur Eržen <aljazerzen@users.noreply.github.com>
2022-07-13 13:00:01 +0200

    prql-macros (#814)

---
+++ prql-macros/src/lib.rs
@@ -0,0 +1,26 @@
+use proc_macro::{Literal, TokenStream, TokenTree};
+use prql_compiler::{compile, format_error};
+use syn::{Expr, ExprLit, Lit};
+
+#[proc_macro]
+pub fn prql(input: TokenStream) -> TokenStream {
+    let input: Expr = syn::parse(input).unwrap();
+
+    let prql_string = match input {
+        Expr::Lit(ExprLit {
+            lit: Lit::Str(lit_str),
+            ..
+        }) => lit_str.value(),
+        _ => panic!("prql! proc macro expected a string"),
+    };
+
+    let sql_string = match compile(&prql_string) {
+        Ok(r) => r,
+        Err(err) => {
+            let (formatted, _) = format_error(err, "<prql_macro>", &prql_string, true);
+            panic!("{}", formatted);
+        }
+    };
+
+    TokenStream::from_iter(vec![TokenTree::Literal(Literal::string(&sql_string))].into_iter())
+}
