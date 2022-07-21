ab7c0291c0b81691357d90dc3e96228544a0739f
Arrizal Amin <arrizalamin@gmail.com>
2022-07-21 11:06:13 +0700

    Add from_json to compiler

--- prql-compiler/src/lib.rs
+++ prql-compiler/src/lib.rs
@@ -40,6 +40,11 @@ pub fn to_json(prql: &str) -> Result<String> {
     Ok(serde_json::to_string(&parse(prql)?)?)
 }
 
+// Convert JSON AST back to PRQL string
+pub fn from_json(json: &str) -> Result<String> {
+    Ok(display(serde_json::from_str(json)?))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
