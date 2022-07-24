9aa7f81240f72af6f507a76f7edb51651c2767d3
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2022-07-24 16:43:38 -0400

    Fix precedence issue (#839)
    * Fix precedence issue
    
    Not yet complete, but solves the immediate issue, and a nice framework to build off, thanks to @alijazerzen
    
    * Complete the BinOps
    
    * .

--- prql-compiler/src/ast/item.rs
+++ prql-compiler/src/ast/item.rs
@@ -70,6 +70,31 @@ pub enum BinOp {
     Coalesce,
 }
 
+impl BinOp {
+    pub fn strength(&self) -> i32 {
+        // Is there some canonical reference for these? I'm guessing we want to
+        // copy SQL for them. At the moment it's just me guessing.
+        match self {
+            BinOp::Coalesce => 15,
+            BinOp::Mod => 11,
+            BinOp::Mul | BinOp::Div => 10,
+            BinOp::Sub | BinOp::Add => 9,
+            BinOp::Gt | BinOp::Lt | BinOp::Gte | BinOp::Lte => 5,
+            BinOp::Eq | BinOp::Ne => 4,
+            BinOp::And | BinOp::Or => 3,
+        }
+    }
+}
+impl Item {
+    pub(crate) fn strength(&self) -> i32 {
+        // TODO: Do we need to include these for more than BinOps?
+        match self {
+            Item::Binary { op, .. } => op.strength(),
+            _ => 20,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, strum::EnumString)]
 pub enum UnOp {
     #[strum(to_string = "-")]
