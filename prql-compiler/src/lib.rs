935960f47a83c8dbef33e267477655528ecd825d
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2022-12-02 01:40:19 -0800

    test: An initial error message test (#1181)
    * tests: An initial error message test
    
    Hopefully we can expand on these; maybe they could be an interesting way for folks to get started.
    
    I left a comment / question in the code about the `FormattedError` struct
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- prql-compiler/src/lib.rs
+++ prql-compiler/src/lib.rs
@@ -49,10 +49,25 @@ pub fn json_to_pl(json: &str) -> Result<Vec<Stmt>> {
 pub fn json_to_rq(json: &str) -> Result<Query> {
     Ok(serde_json::from_str(json)?)
 }
+
+// TODO: possibly collapse this with other functions. Deliberately not `pub`
+// atm.
+#[allow(dead_code)]
+fn compile_with_formatted_error(prql: &str) -> Result<String, FormattedError> {
+    let res = compile(prql);
+    match res {
+        Ok(buf) => Ok(buf),
+        // TODO: should this info be a method on our standard `Error` type; e.g.
+        // we can call `error.formatted`? Could it already have the `source` &
+        // `source_id` or does that need to be passed in separately?
+        Err(e) => Err(error::format_error(e, "", prql, false)),
+    }
+}
+
 // Simple tests for "this PRQL creates this SQL" go here.
 #[cfg(test)]
 mod test {
-    use crate::{json_to_pl, parse, pl_to_json, pl_to_prql};
+    use crate::{compile_with_formatted_error, json_to_pl, parse, pl_to_json, pl_to_prql};
 
     use super::compile;
     use insta::{assert_display_snapshot, assert_snapshot};
@@ -1714,6 +1729,25 @@ join y [foo == only_in_x]
         );
     }
 
+    #[test]
+    /// Start testing some error messages. This can hopefully be expanded significantly.
+    fn test_errors() {
+        assert_display_snapshot!(compile_with_formatted_error(r###"
+        from x
+        select a
+        select b
+        "###).unwrap_err().message.split('\n').map(str::trim).collect::<Vec<_>>().join("\n"),
+            @r###"
+        Error:
+        ╭─[:4:16]
+        │
+        4 │         select b
+        ·                ┬
+        ·                ╰── Unknown name b
+        ───╯
+        "###
+        );
+    }
     #[test]
     fn test_toposort() {
         // #1183
