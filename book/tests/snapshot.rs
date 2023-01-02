4c480e66ed37729da1b8368895d21450f66c9ffb
Maximilian Roos <m@maxroos.com>
2023-01-02 14:25:28 -0800

    test: Include PRQL source in snapshot test files
    There's a way of doing this which is well-supported by insta, so using that
    
    I'm going to split the changes into multiple commits, since the snapshot tests will show lots of diffs

--- book/tests/snapshot.rs
+++ book/tests/snapshot.rs
@@ -5,7 +5,7 @@
 /// - Replaces the PRQL code block with a comparison table.
 ///
 /// We also use this test to run tests on our Display trait output, currently as
-/// another set of snapshots.
+/// another set of snapshots (more comments inline).
 //
 // Overall, this is overengineered — it's complicated and took a long time to
 // write. The intention is good — have a version of the SQL that's committed
@@ -132,16 +132,24 @@ fn run_reference_prql() {
             return;
         }
 
-        println!("{:?}", path);
-
         let opts = sql::Options::default().no_signature().some();
         let sql = compile(&prql, opts).unwrap_or_else(|e| format!("{prql}\n\n{e}"));
-        assert_snapshot!(sql);
+        // `glob!` gives us the file path in the test name anyway, so we pass an
+        // empty name. We pass `&prql` so the prql is in the snapshot (albeit in
+        // a single line, and, in the rare case that the SQL doesn't change, the
+        // PRQL only updates on running cargo insta with `--force-update-snapshots`).
+        assert_snapshot!("", &sql, &prql);
     });
 }
 
+
 /// Snapshot the display trait output of each example.
 // Currently not a separate test, see notes in caller.
+//
+// TODO: this involves writing out almost the same PRQL again — instead we could
+// compare the output of Display to the auto-formatted source. But we need an
+// autoformatter for that (unless we want to raise on any non-matching input,
+// which seems very strict)
 fn run_display_reference_prql() {
     glob!("prql/**/*.prql", |path| {
         let prql = fs::read_to_string(path).unwrap();
