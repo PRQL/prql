523817a9d9cb071a72de12c418424288563a7c93
Paul Roskell <blurrech@gmail.com>
2023-01-07 01:29:19 +0000

    revert: simplify playground operators highlighting list.
    This reverts commit 2e9947447bb00f22def7da415dc5b46e35049a1c.

--- playground/src/workbench/prql-syntax.js
+++ playground/src/workbench/prql-syntax.js
@@ -15,31 +15,35 @@ const TRANSFORMS = [
 const BUILTIN_FUNCTIONS = ["switch"]; // "in", "as"
 const KEYWORDS = ["func", "table", "prql"];
 const LITERALS = ["null", "true", "false"];
-const OPERATORS = [
-  "-",
-  "*",
-  "/",
-  "%",
-  "+",
-  "-",
-  "==",
-  "!=",
-  ">",
-  "<",
-  ">=",
-  "<=",
-  "??",
-  "and",
-  "or",
-]; // "not"
+const OPERATORS = ["and", "or"]; // "not"
 
 const def = {
   // Set defaultToken to invalid to see what you do not tokenize yet
   // defaultToken: 'invalid',
 
-  keywords: [...TRANSFORMS, ...BUILTIN_FUNCTIONS, ...KEYWORDS, ...LITERALS],
+  keywords: [
+    ...TRANSFORMS,
+    ...BUILTIN_FUNCTIONS,
+    ...KEYWORDS,
+    ...LITERALS,
+    ...OPERATORS,
+  ],
 
-  operators: OPERATORS,
+  operators: [
+    "-",
+    "*",
+    "/",
+    "%",
+    "+",
+    "-",
+    "==",
+    "!=",
+    ">",
+    "<",
+    ">=",
+    "<=",
+    "??",
+  ],
 
   // The main tokenizer for our languages
   tokenizer: {
