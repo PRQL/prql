f8b7c513e98353f964e62c47941b4d198941aead
Kasun Vithanage <alankasun@gmail.com>
2023-01-14 02:49:54 +0530

    (prql-elixir): wip tests

--- prql_elixir/test/prql_test.exs
+++ prql_elixir/test/prql_test.exs
@@ -2,7 +2,11 @@ defmodule PRQLTest do
   use ExUnit.Case
   doctest PRQL
 
-  test "greets the world" do
-    assert PRQL.hello() == :world
+  test "compiles to correct PRQL query" do
+    prql = """
+      from customers
+    """
+
+    assert 1 == 1
   end
 end
