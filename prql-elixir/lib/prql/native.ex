1a4b03736e87708e355c3ca9d77de9f0b030463b
Kasun Vithanage <alankasun@gmail.com>
2023-01-14 14:17:28 +0530

    feat: Elixir Bindings for PRQL (#1500)
    * (prql-elixir): initialize the elixir project
    
    * (prql-elixir): added nif module
    
    * (prql-elixir): finalize the nif integration
    
    * (prql-elixir): added ex_doc
    
    * (prql-elixir): added doc test
    
    * (prql-elixir): finalize native module
    
    * (prql-elixir): wip docs
    
    * (prql-elixir): wip tests
    
    * (prql-elixir): updated docs
    
    * (prql-elixir): added unit test
    
    * (prql-elixir): update readme
    
    * (prql-elixir): update directory name
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * chore: update author on cargo package
    
    * chore: remove redundant .gitignore
    
    * chore: added elixir tests to the task file
    
    * chore: update the elixir crate version
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: build task for elixir
    
    * fix: use single words for dialects
    
    * fix: simplify the compile function
    
    * fix: simplify the options casting
    
    * fix: disable signature comment for tests
    
    * ci: added test-elirix workflow
    
    * ci: fix script
    
    * ci: rename task
    
    * pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ci: disable autocrlf
    
    * chore: added todo
    
    * ci: try disable crlf
    
    * ci: fix working dir
    
    * ci: correct cache paths
    
    * pre-commit
    
    * docs: added development instructions to prql-elixir
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update prql-elixir/README.md
    
    * Update prql-elixir/README.md
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * docs: readme update
    
    * docs: fixed nif readme
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Maximilian Roos <m@maxroos.com>
    Co-authored-by: Maximilian Roos <5635139+max-sixty@users.noreply.github.com>

---
+++ prql-elixir/lib/prql/native.ex
@@ -0,0 +1,37 @@
+defmodule PRQL.Native do
+  @moduledoc false
+  use Rustler, otp_app: :prql
+
+  def compile(_prql_query, _options), do: :erlang.nif_error(:nif_not_loaded)
+
+  def prql_to_pl(_prql_query), do: :erlang.nif_error(:nif_not_loaded)
+
+  def pl_to_rq(_pl_json), do: :erlang.nif_error(:nif_not_loaded)
+
+  def rq_to_sql(_rq_json), do: :erlang.nif_error(:nif_not_loaded)
+end
+
+defmodule PRQL.Native.CompileOptions do
+  @typedoc """
+  Dialect used for SQL generation
+  """
+  @type dialect() ::
+          :generic
+          | :mssql
+          | :mysql
+          | :postgres
+          | :ansi
+          | :bigquery
+          | :click_house
+          | :hive
+          | :sqllite
+          | :snowflake
+
+  @type t :: %__MODULE__{
+          dialect: dialect(),
+          format: boolean(),
+          signature_comment: boolean()
+        }
+
+  defstruct dialect: :generic, format: true, signature_comment: true
+end
---
+++ prql-elixir/native/prql/src/lib.rs
@@ -0,0 +1,154 @@
+use rustler::{Atom, NifResult, NifStruct, NifTuple};
+
+mod atoms {
+    rustler::atoms! {
+      ok,
+      error,
+
+      // dialects
+      ansi,
+      bigquery,
+      clickhouse,
+      generic,
+      hive,
+      mssql,
+      mysql,
+      postgres,
+      sqllite,
+      snowflake
+    }
+}
+
+/// Convert a `Result` from PRQL into a tuple in elixir `{:ok, binary()} | {:error, binary()}`
+fn to_result_tuple(result: Result<String, prql_compiler::ErrorMessages>) -> NifResult<Response> {
+    match result {
+        Ok(sql) => Ok(Response {
+            status: atoms::ok(),
+            result: sql,
+        }),
+        Err(e) => Ok(Response {
+            status: atoms::error(),
+            result: e.to_json(),
+        }),
+    }
+}
+
+/// Get the dialect from an atom. By default `Generic` dialect will be used
+fn dialect_from_atom(a: Atom) -> prql_compiler::sql::Dialect {
+    use prql_compiler::sql::Dialect as D;
+
+    if a == atoms::ansi() {
+        D::Ansi
+    } else if a == atoms::bigquery() {
+        D::BigQuery
+    } else if a == atoms::clickhouse() {
+        D::ClickHouse
+    } else if a == atoms::generic() {
+        D::Generic
+    } else if a == atoms::hive() {
+        D::Hive
+    } else if a == atoms::mssql() {
+        D::MsSql
+    } else if a == atoms::mysql() {
+        D::MySql
+    } else if a == atoms::postgres() {
+        D::PostgreSql
+    } else if a == atoms::sqllite() {
+        D::SQLite
+    } else if a == atoms::snowflake() {
+        D::Snowflake
+    } else {
+        D::Generic
+    }
+}
+
+impl From<CompileOptions> for prql_compiler::sql::Options {
+    /// Get `prql_compiler::sql::Options` options from `CompileOptions`
+    fn from(o: CompileOptions) -> Self {
+        prql_compiler::sql::Options {
+            format: o.format,
+            dialect: Some(dialect_from_atom(o.dialect)),
+            signature_comment: o.signature_comment,
+        }
+    }
+}
+
+#[derive(Clone, NifStruct, Debug)]
+#[module = "PRQL.Native.CompileOptions"]
+pub struct CompileOptions {
+    /// Pass generated SQL string trough a formatter that splits it
+    /// into multiple lines and prettifies indentation and spacing.
+    ///
+    /// Defaults to true.
+    pub format: bool,
+
+    /// Target dialect you want to compile for.
+    ///
+    /// Because PRQL compiles to a subset of SQL, not all SQL features are
+    /// required for PRQL. This means that generic dialect may work with most
+    /// databases.
+    ///
+    /// If something does not work in dialect you need, please report it at
+    /// GitHub issues.
+    ///
+    /// If None is used, `sql_dialect` flag from query definition is used.
+    /// If it does not exist, [Dialect::Generic] is used.
+    pub dialect: Atom,
+
+    /// Emits the compiler signature as a comment after generated SQL
+    ///
+    /// Defaults to true.
+    pub signature_comment: bool,
+}
+
+#[derive(NifTuple)]
+pub struct Response {
+    /// status atom `:ok` or `:error`
+    status: Atom,
+
+    /// result string
+    result: String,
+}
+
+#[rustler::nif]
+/// compile a prql query into sql
+pub fn compile(prql_query: &str, options: CompileOptions) -> NifResult<Response> {
+    let result = prql_compiler::compile(prql_query, Some(options.into()));
+    to_result_tuple(result)
+}
+
+#[rustler::nif]
+/// convert a prql query into PL AST
+pub fn prql_to_pl(prql_query: &str) -> NifResult<Response> {
+    to_result_tuple(
+        Ok(prql_query)
+            .and_then(prql_compiler::prql_to_pl)
+            .and_then(prql_compiler::json::from_pl),
+    )
+}
+
+#[rustler::nif]
+/// Convert PL AST into RQ
+pub fn pl_to_rq(pl_json: &str) -> NifResult<Response> {
+    to_result_tuple(
+        Ok(pl_json)
+            .and_then(prql_compiler::json::to_pl)
+            .and_then(prql_compiler::pl_to_rq)
+            .and_then(prql_compiler::json::from_rq),
+    )
+}
+
+#[rustler::nif]
+/// Convert RQ to SQL
+pub fn rq_to_sql(rq_json: &str) -> NifResult<Response> {
+    to_result_tuple(
+        Ok(rq_json)
+            .and_then(prql_compiler::json::to_rq)
+            .and_then(|x| prql_compiler::rq_to_sql(x, None)),
+    )
+}
+
+rustler::init!(
+    "Elixir.PRQL.Native",
+    [compile, prql_to_pl, pl_to_rq, rq_to_sql]
+);
