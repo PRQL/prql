e159a17cf854924637d198f263abf7ef8919dd7c
Kasun Vithanage <alankasun@gmail.com>
2023-01-15 13:56:38 +0530

    feat: added alternative exception based functions to call prql methods

---
+++ prql-elixir/lib/prql/errors.ex
@@ -0,0 +1,13 @@
+defmodule PRQL.PRQLError do
+  @moduledoc """
+  Represents an error returned from PRQL compiler.
+  
+  `:error` contains the message from compiler as a **JSON string**.
+  """
+  defexception [:message, :error]
+
+  @impl true
+  def exception(err) do
+    %__MODULE__{message: "Error compiling PRQL query", error: err}
+  end
+end
