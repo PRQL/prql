071a51f2044b149db71254d52991b4fd21e99082
Nicholas George <nicholas.george32@gmail.com>
2023-02-07 09:34:54 -0500

    Run all query tests in a loop

--- prql-python/python/tests/conftest.py
+++ prql-python/python/tests/conftest.py
@@ -5,9 +5,9 @@ import yaml
 
 @pytest.fixture()
 def example_queries():
-    book_path = "../website/content/_index.md"
-    with open(book_path, "r") as f:
-        book = f.read()
-    book_yaml = yaml.safe_load(book.replace("---", ""))
-    showcase_section = book_yaml["showcase_section"]["examples"]
+    website_path = "../website/content/_index.md"
+    with open(website_path, "r") as f:
+        website = f.read()
+    website_yaml = yaml.safe_load(website.replace("---", ""))
+    showcase_section = website_yaml["showcase_section"]["examples"]
     return showcase_section
