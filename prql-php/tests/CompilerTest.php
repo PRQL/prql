3db2a06474b15d358407572285f44c69d18112ab
Jonathan <vanillajonathan@users.noreply.github.com>
2023-02-19 20:10:33 +0100

    Add more unit tests

--- prql-php/tests/CompilerTest.php
+++ prql-php/tests/CompilerTest.php
@@ -5,6 +5,23 @@ use PHPUnit\Framework\TestCase;
 
 final class CompilerTest extends TestCase
 {
+    public function testFfiExtensionIsLoaded(): void
+    {
+        $this->assertTrue(extension_loaded("ffi"));
+    }
+
+    public function testPrqlLibraryFileExists(): void
+    {
+        $this->assertFileExists("src/libprql_lib.so");
+    }
+
+    public function testPrqlLibraryLoads(): void
+    {
+        $code = "int to_sql(char *prql_query, char *sql_query);";
+        $ffi = FFI::cdef($code, "src/libprql_lib.so");
+        $this->assertInstanceOf(FFI::class, $ffi);
+    }
+
     public function testInvalidQueryThrows(): void
     {
         $this->expectException(\InvalidArgumentException::class);
