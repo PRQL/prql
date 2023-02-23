cc6b066ce840d52590742efd3558fd475b3282e5
Jonathan <vanillajonathan@users.noreply.github.com>
2023-02-23 14:05:32 +0100

    Update libprql_lib.h

--- prql-lib/libprql_lib.h
+++ prql-lib/libprql_lib.h
@@ -3,6 +3,33 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct CompileOptions {
+  /**
+   * Pass generated SQL string trough a formatter that splits it
+   * into multiple lines and prettifies indentation and spacing.
+   *
+   * Defaults to true.
+   */
+  bool format;
+  /**
+   * Target and dialect to compile to.
+   */
+  const char *target;
+  /**
+   * Emits the compiler signature as a comment after generated SQL
+   *
+   * Defaults to true.
+   */
+  bool signature_comment;
+} CompileOptions;
+
+/**
+ * # Safety
+ *
+ * This function is inherently unsafe because it is using C ABI.
+ */
+const char *compile(const char *query, struct CompileOptions options);
+
 /**
  * # Safety
  *
