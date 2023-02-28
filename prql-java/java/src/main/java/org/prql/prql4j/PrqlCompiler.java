21d247590214cefdf421ca31da1816d9dde0b406
Libing Chen <libing.chen@gmail.com>
2023-03-01 01:08:32 +0800

    feat: add dialect, format and signature parameters for toSql method

--- prql-java/java/src/main/java/org/prql/prql4j/PrqlCompiler.java
+++ prql-java/java/src/main/java/org/prql/prql4j/PrqlCompiler.java
@@ -3,7 +3,7 @@ package org.prql.prql4j;
 import java.io.IOException;
 
 public class PrqlCompiler {
-    public static native String toSql(String query) throws Exception;
+    public static native String toSql(String query, String dialect, boolean format, boolean signature) throws Exception;
     public static native String toJson(String query) throws Exception;
     public static native String format(String query) throws Exception;
 
--- prql-java/src/lib.rs
+++ prql-java/src/lib.rs
@@ -1,7 +1,8 @@
 use jni::objects::{JClass, JString};
-use jni::sys::jstring;
+use jni::sys::{jboolean, jstring};
 use jni::JNIEnv;
-use prql_compiler::{json, prql_to_pl, pl_to_prql, Options, ErrorMessages};
+use prql_compiler::{json, prql_to_pl, pl_to_prql, Options, ErrorMessages, Target};
+use prql_compiler::sql::Dialect;
 
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -9,12 +10,38 @@ pub extern "system" fn Java_org_prql_prql4j_PrqlCompiler_toSql(
     env: JNIEnv,
     _class: JClass,
     query: JString,
+    dialect: JString,
+    format: jboolean,
+    signature: jboolean,
 ) -> jstring {
     let prql_query: String = env
         .get_string(query)
         .expect("Couldn't get java string!")
         .into();
-    let result = prql_compiler::compile(&prql_query, &Options::default());
+    let prql_dialect: String = env
+        .get_string(dialect)
+        .expect("Couldn't get java string!")
+        .into();
+    let dialect = match prql_dialect.as_str() {
+        "ansi" => Dialect::Ansi,
+        "bigquery" => Dialect::BigQuery,
+        "clickhouse" => Dialect::ClickHouse,
+        "duckdb" => Dialect::DuckDb,
+        "generic" => Dialect::Generic,
+        "hive" => Dialect::Hive,
+        "mssql" => Dialect::MsSql,
+        "mysql" => Dialect::MySql,
+        "postgres" => Dialect::PostgreSql,
+        "sqlite" => Dialect::SQLite,
+        "snowflake" => Dialect::Snowflake,
+        _ => Dialect::Generic
+    };
+    let opt = Options {
+        format: format != 0,
+        target: Target::Sql(Some(dialect)),
+        signature_comment: signature != 0,
+    };
+    let result = prql_compiler::compile(&prql_query, &opt);
     java_string_with_exception(result, &env)
 }
 
