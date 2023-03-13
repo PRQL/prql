642ece4a22ca26598578c29012dbb57df00cdbd5
Jonathan <vanillajonathan@users.noreply.github.com>
2023-03-13 16:12:29 +0100

    Update NativeResult.cs

--- bindings/prql-dotnet/PrqlCompiler/NativeResult.cs
+++ bindings/prql-dotnet/PrqlCompiler/NativeResult.cs
@@ -4,10 +4,10 @@ namespace Prql.Compiler
 {
     internal struct NativeResult
     {
-#pragma warning disable CS0649
+#pragma warning disable CS0649 // Field is never assigned to
         public string Output;
         public IntPtr Messages;
         public int MessagesLen;
-#pragma warning restore CS0649
+#pragma warning restore CS0649 // Field is never assigned to
     }
 }
