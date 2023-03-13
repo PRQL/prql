72767d9462b7a2ae6eb09fc28e142dce52c76dfa
Jonathan <vanillajonathan@users.noreply.github.com>
2023-03-13 10:51:24 +0100

    Update NativeResult.cs

--- bindings/prql-dotnet/PrqlCompiler/NativeResult.cs
+++ bindings/prql-dotnet/PrqlCompiler/NativeResult.cs
@@ -4,8 +4,10 @@ namespace Prql.Compiler
 {
     internal struct NativeResult
     {
+#pragma warning disable CS2141
         public string Output;
         public IntPtr Messages;
         public int MessagesLen;
+#pragma warning restore CS2141
     }
 }
