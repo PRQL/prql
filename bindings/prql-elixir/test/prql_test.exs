5eb4063dbd36bdac07529797dd2cec8c55127263
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-03-25 23:01:06 -0700

    chore: Fix elixir test (#2311)
    * chore: Fix elixir test
    
    * .
    
    * GPT-4 fixes the test

--- bindings/prql-elixir/test/prql_test.exs
+++ bindings/prql-elixir/test/prql_test.exs
@@ -15,9 +15,38 @@ defmodule PRQLTest do
   end
 
   test "return errors on invalid query" do
-    excepted_result =
-      "{\"inner\":[{\"code\":null,\"reason\":\"Unknown name invalid\",\"hint\":null,\"span\":{\"start\":0,\"end\":7},\"display\":\"Error: \\n   ╭─[:1:1]\\n   │\\n 1 │ invalid\\n   │ ───┬───  \\n   │    ╰───── Unknown name invalid\\n───╯\\n\",\"location\":{\"start\":[0,0],\"end\":[0,7]}}]}"
+    expected_result = ~S"""
+    {
+      "inner": [
+        {
+          "kind": "Error",
+          "code": null,
+          "reason": "Unknown name invalid",
+          "hint": null,
+          "span": {
+            "start": 0,
+            "end": 7
+          },
+          "display": "Error: \n   ╭─[:1:1]\n   │\n 1 │ invalid\n   │ ───┬───  \n   │    ╰───── Unknown name invalid\n───╯\n",
+          "location": {
+            "start": [0, 0],
+            "end": [0, 7]
+          }
+        }
+      ]
+    }
+    """
 
-    assert PRQL.compile("invalid", @compile_opts) == {:error, excepted_result}
+    {:ok, expected_json} = Jason.decode(expected_result)
+    result = PRQL.compile("invalid", @compile_opts)
+
+    case result do
+      {:error, error_string} ->
+        {:ok, error_json} = Jason.decode(error_string)
+        assert error_json == expected_json
+
+      _ ->
+        flunk("Expected an error tuple")
+    end
   end
 end
