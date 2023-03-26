80e6b18faaec3afbcb075ac01745a92462dc1d38
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-03-26 00:05:20 -0700

    chore: Update prql-elixir readme (#2312)
    Also another pass of `prql_test.exs` through GPT

--- bindings/prql-elixir/test/prql_test.exs
+++ bindings/prql-elixir/test/prql_test.exs
@@ -5,39 +5,42 @@ defmodule PRQLTest do
   @compile_opts [signature_comment: false]
 
   test "compiles PRQL" do
-    prql_query = """
-      from customers
-    """
+    prql_query = "from customers"
 
-    excepted_result = "SELECT\n  *\nFROM\n  customers\n"
-
-    assert PRQL.compile(prql_query, @compile_opts) == {:ok, excepted_result}
+    assert PRQL.compile(prql_query, @compile_opts) ==
+             {:ok,
+              """
+              SELECT
+                *
+              FROM
+                customers
+              """}
   end
 
   test "return errors on invalid query" do
-    expected_result = ~S"""
-    {
-      "inner": [
-        {
-          "kind": "Error",
-          "code": null,
-          "reason": "Unknown name invalid",
-          "hint": null,
-          "span": {
-            "start": 0,
-            "end": 7
-          },
-          "display": "Error: \n   ╭─[:1:1]\n   │\n 1 │ invalid\n   │ ───┬───  \n   │    ╰───── Unknown name invalid\n───╯\n",
-          "location": {
-            "start": [0, 0],
-            "end": [0, 7]
+    {:ok, expected_json} =
+      Jason.decode(~S"""
+      {
+        "inner": [
+          {
+            "kind": "Error",
+            "code": null,
+            "reason": "Unknown name invalid",
+            "hint": null,
+            "span": {
+              "start": 0,
+              "end": 7
+            },
+            "display": "Error: \n   ╭─[:1:1]\n   │\n 1 │ invalid\n   │ ───┬───  \n   │    ╰───── Unknown name invalid\n───╯\n",
+            "location": {
+              "start": [0, 0],
+              "end": [0, 7]
+            }
           }
-        }
-      ]
-    }
-    """
+        ]
+      }
+      """)
 
-    {:ok, expected_json} = Jason.decode(expected_result)
     result = PRQL.compile("invalid", @compile_opts)
 
     case result do
