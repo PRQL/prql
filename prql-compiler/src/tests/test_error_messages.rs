3281b2437bf2773fe7d633462e1aa2497d1a9114
Maximilian Roos <m@maxroos.com>
2023-03-28 19:02:32 -0700

    fix: Raise an error on unavailable excludes
    Closes #2292. It was here deliberately (the old code was _more_ work). I would think it's better to raise here — getting this wrong is still wrong...

--- prql-compiler/src/tests/test_error_messages.rs
+++ prql-compiler/src/tests/test_error_messages.rs
@@ -126,3 +126,15 @@ fn test_hint_missing_args() {
     ───╯
     "###)
 }
+
+#[test]
+fn test_missing_exclude() {
+    // Not a great error message, would be better to have a span & a better
+    // name. Not quite bad enough to put in `bad_error_messages` yet though...
+    assert_display_snapshot!(compile(r###"
+    from tracks
+    select ![milliseconds,bytes]
+    "###).unwrap_err(), @r###"
+    Error: Excluding columns (milliseconds, bytes) is not supported by the current dialect, GenericDialect
+    "###)
+}
