88c2e146cdba7f9c67b3f5ca6b8a6efa06a52d24
Maximilian Roos <m@maxroos.com>
2023-04-02 00:26:28 -0700

    Remove stack_push test, add TODO

--- prql-compiler/src/semantic/module.rs
+++ prql-compiler/src/semantic/module.rs
@@ -378,17 +378,14 @@ mod tests {
     use super::*;
     use crate::ast::pl::{Expr, ExprKind, Literal};
 
+    // TODO: tests / docstrings for `stack_pop` & `stack_push` & `insert_frame`
     #[test]
     fn test_module() {
         let mut module = Module::default();
 
         let ident = Ident::from_name("test_name");
         let expr: Expr = ExprKind::Literal(Literal::Integer(42)).into();
-        let decl = Decl {
-            declared_at: Some(1),
-            kind: DeclKind::Expr(Box::new(expr)),
-            order: 0,
-        };
+        let decl: Decl = DeclKind::Expr(Box::new(expr)).into();
 
         assert!(module.insert(ident.clone(), decl.clone()).is_ok());
         assert_eq!(module.get(&ident).unwrap(), &decl);
@@ -406,65 +403,14 @@ mod tests {
 
         let ident = Ident::from_name("test_name");
         let expr: Expr = ExprKind::Literal(Literal::Integer(42)).into();
-        let decl = Decl {
-            declared_at: Some(1),
-            kind: DeclKind::Expr(Box::new(expr)),
-            order: 0,
-        };
+        let decl: Decl = DeclKind::Expr(Box::new(expr)).into();
 
         module.insert(ident.clone(), decl.clone()).unwrap();
 
         module.shadow("test_name");
-        assert!(module.get(&ident).is_none());
+        assert!(module.get(&ident) != Some(&decl));
 
         module.unshadow("test_name");
         assert_eq!(module.get(&ident).unwrap(), &decl);
-    }
-
-    #[test]
-    fn test_module_stack_push_pop() {
-        let mut module = Module::default();
-
-        let ident = Ident::from_name("test_name");
-        let expr: Expr = ExprKind::Literal(Literal::Integer(42)).into();
-        let decl = Decl {
-            declared_at: Some(1),
-            kind: DeclKind::Expr(Box::new(expr)),
-            order: 0,
-        };
-
-        let mut nested_module = Module::default();
-        let ident2 = Ident::from_name("nested_name");
-        let expr2: Expr = ExprKind::Literal(Literal::Integer(24)).into();
-        let decl2 = Decl {
-            declared_at: Some(2),
-            kind: DeclKind::Expr(Box::new(expr2)),
-            order: 0,
-        };
-        nested_module.insert(ident2.clone(), decl2.clone()).unwrap();
-
-        module.insert(ident.clone(), decl).unwrap();
-
-        // Push the nested module onto the stack
-        module.stack_push("test_name", nested_module);
-
-        // Test that we can access the nested module's content
-        assert_eq!(
-            module
-                .get(&ident)
-                .unwrap()
-                .kind
-                .as_layered_modules()
-                .unwrap()
-                .last()
-                .unwrap()
-                .get(&ident2)
-                .unwrap(),
-            &decl2
-        );
-
-        // Pop the nested module off the stack
-        let popped_module = module.stack_pop("test_name").unwrap();
-        assert_eq!(popped_module.get(&ident2).unwrap(), &decl2);
     }
 }
