6ce9bdfdc4431142b3f92fc57bf70018b10c0b24
Aljaž Mur Eržen <aljaz.erzen@gmail.com>
2023-04-13 15:05:46 +0200

    Revert to checkpoint 1

--- prql-compiler/src/sql/std.rs
+++ prql-compiler/src/sql/std.rs
@@ -100,27 +100,26 @@ pub const STD_COALESCE: FunctionDecl<2> = FunctionDecl::new("std.coalesce");
 pub fn try_unpack<const ARG_COUNT: usize>(
     expr: rq::Expr,
     decl: FunctionDecl<ARG_COUNT>,
-) -> Result<[rq::Expr; ARG_COUNT], rq::Expr> {
+) -> Result<Result<[rq::Expr; ARG_COUNT], rq::Expr>> {
     if let rq::ExprKind::BuiltInFunction { name, args } = &expr.kind {
         if decl.name == name {
             let (_, args) = expr.kind.into_built_in_function().unwrap();
 
             let args: [rq::Expr; ARG_COUNT] = args
                 .try_into()
-                .map_err(|_| anyhow::anyhow!("Bad usage of function {}", decl.name))
-                .unwrap();
+                .map_err(|_| anyhow::anyhow!("Bad usage of function {}", decl.name))?;
 
-            return Ok(args);
+            return Ok(Ok(args));
         }
     }
-    Err(expr)
+    Ok(Err(expr))
 }
 
 pub fn try_unpack_with<const ARG_COUNT: usize, M, T>(
     expr: rq::Expr,
     decl: FunctionDecl<ARG_COUNT>,
     mapper: M,
-) -> Result<T, rq::Expr>
+) -> Result<Result<T, rq::Expr>>
 where
     M: FnOnce([rq::Expr; ARG_COUNT]) -> Result<T, [rq::Expr; ARG_COUNT]>,
 {
@@ -130,10 +129,9 @@ where
 
             let args: [rq::Expr; ARG_COUNT] = args
                 .try_into()
-                .map_err(|_| anyhow::anyhow!("Bad usage of function {}", decl.name))
-                .unwrap();
+                .map_err(|_| anyhow::anyhow!("Bad usage of function {}", decl.name))?;
 
-            return match mapper(args) {
+            return Ok(match mapper(args) {
                 Ok(res) => Ok(res),
                 Err(args) => {
                     // mapper was unsuccessful, let's repack back the original Expr
@@ -144,8 +142,8 @@ where
                         ..expr
                     })
                 }
-            };
+            });
         }
     }
-    Err(expr)
+    Ok(Err(expr))
 }
