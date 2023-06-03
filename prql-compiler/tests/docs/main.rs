52380c579105f8718bf01ce201b243493f4855bd
eitsupi <ts1s1andn@gmail.com>
2023-06-03 15:04:40 +0000

    test: add a test for website hero example

--- prql-compiler/tests/docs/main.rs
+++ prql-compiler/tests/docs/main.rs
@@ -14,20 +14,38 @@ fn sql_normalize(sql: &str) -> String {
     re.replace_all(sql, " ").trim().to_string()
 }
 
-fn website_examples() -> Vec<Value> {
-    let website_contents = fs::read_to_string(WEBSITE_TOPPAGE)
+fn website_contents() -> Value {
+    let contents = fs::read_to_string(WEBSITE_TOPPAGE)
         .unwrap()
         .replace("---", "");
-    let yaml = serde_yaml::from_str::<Value>(&website_contents).unwrap();
-    let examples = yaml
+
+    serde_yaml::from_str::<Value>(&contents).unwrap()
+}
+
+fn website_examples() -> Vec<Value> {
+    let value = website_contents();
+
+    value
         .get("showcase_section")
         .unwrap()
         .get("examples")
         .unwrap()
         .as_sequence()
-        .unwrap();
+        .unwrap()
+        .to_vec()
+}
 
-    examples.to_vec()
+fn website_hero_example() -> String {
+    let value = website_contents();
+
+    value
+        .get("hero_section")
+        .unwrap()
+        .get("prql_example")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_string()
 }
 
 #[test]
@@ -37,4 +55,10 @@ fn test_website_examples() {
         let sql = example.get("sql").unwrap().as_str().unwrap();
         assert_eq!(sql_normalize(&compile(prql).unwrap()), sql_normalize(sql));
     }
+}
+
+#[test]
+fn test_website_hero_example() {
+    let prql = website_hero_example();
+    compile(&prql).unwrap();
 }
