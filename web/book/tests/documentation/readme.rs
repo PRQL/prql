ce36fc6c6b6db643366f59078ca5e183c49d160c
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-06-05 16:00:20 -0700

    test: Add comments re CRLF, make test robust (#2741)

--- web/book/tests/documentation/readme.rs
+++ web/book/tests/documentation/readme.rs
@@ -6,7 +6,7 @@ use super::compile;
 fn test_readme_examples() {
     let contents = include_str!("../../../../README.md");
     // Similar to code at https://github.com/PRQL/prql/blob/65706a115a84997c608eaeda38b1aef1240fcec3/web/book/tests/snapshot.rs#L152, but specialized for the Readme.
-    let re = Regex::new(r"(?s)```(elm|prql)\n(?P<prql>.+?)\n```").unwrap();
+    let re = Regex::new(r"(?s)```(elm|prql)\r?\n(?P<prql>.+?)\r?\n```").unwrap();
     assert_ne!(re.find_iter(contents).count(), 0);
     re.captures_iter(contents).for_each(|capture| {
         compile(&capture["prql"]).unwrap();
