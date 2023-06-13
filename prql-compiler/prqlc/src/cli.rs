f8e7e6baaa3458706b9e916f3bde0a638c71ce7f
Aljaž Mur Eržen <aljaz@edgedb.com>
2023-06-13 14:21:49 +0200

    fix

--- prql-compiler/prqlc/src/cli.rs
+++ prql-compiler/prqlc/src/cli.rs
@@ -60,17 +60,8 @@ enum Command {
         input: clio_extended::Input,
     },
 
-    /// Parse & evaluate & generate PRQL code back
-    // TODO: this is not meant to be used apart from language design helper
-    // it is not polished (will panic a lot), so should we prefix with `debug:`?
-    #[command(name = "eval")]
-    Eval(IoArgs),
-
-    /// Parse, resolve & combine source with comments annotating relation type
-    Annotate(IoArgs),
-
-    /// Parse & resolve, but don't lower into RQ
-    Debug(IoArgs),
+    #[command(subcommand)]
+    Debug(DebugCommand),
 
     /// Parse, resolve & lower into RQ
     Resolve {
@@ -122,6 +113,22 @@ enum Command {
         #[arg(value_enum)]
         shell: clap_complete_command::Shell,
     },
+}
+
+/// Commands for meant for debugging, prone to change
+#[derive(Subcommand, Debug, Clone)]
+pub enum DebugCommand {
+    /// Parse & resolve, but don't lower into RQ
+    Semantics(IoArgs),
+
+    /// Parse & evaluate expression down to a value
+    ///
+    /// Cannot contain references to tables or any other outside sources.
+    /// Meant as a playground for testing out language design decisions.
+    Eval(IoArgs),
+
+    /// Parse, resolve & combine source with comments annotating relation type
+    Annotate(IoArgs),
 }
 
 #[derive(clap::Args, Default, Debug, Clone)]
@@ -224,7 +231,7 @@ impl Command {
                     Format::Yaml => serde_yaml::to_string(&ast)?.into_bytes(),
                 }
             }
-            Command::Debug(_) => {
+            Command::Debug(DebugCommand::Semantics(_)) => {
                 semantic::load_std_lib(sources);
                 let stmts = prql_to_pl_tree(sources)?;
 
@@ -241,7 +248,7 @@ impl Command {
                 out.extend(format!("\n{context:#?}\n").into_bytes());
                 out
             }
-            Command::Annotate(_) => {
+            Command::Debug(DebugCommand::Annotate(_)) => {
                 let (_, source) = sources.sources.clone().into_iter().exactly_one().or_else(
                     |_| bail!(
                         "Currently `annotate` only works with a single source, but found multiple sources: {:?}",
@@ -271,7 +278,7 @@ impl Command {
                 // combine with source
                 combine_prql_and_frames(&source, frames).as_bytes().to_vec()
             }
-            Command::Eval(_) => {
+            Command::Debug(DebugCommand::Eval(_)) => {
                 let stmts = prql_to_pl_tree(sources)?;
 
                 let mut res = String::new();
@@ -366,9 +373,9 @@ impl Command {
             | SQLCompile { io_args, .. }
             | SQLPreprocess(io_args)
             | SQLAnchor { io_args, .. }
-            | Debug(io_args)
-            | Eval(io_args)
-            | Annotate(io_args) => io_args,
+            | Debug(DebugCommand::Semantics(io_args))
+            | Debug(DebugCommand::Annotate(io_args))
+            | Debug(DebugCommand::Eval(io_args)) => io_args,
             _ => unreachable!(),
         };
         let input = &mut io_args.input;
@@ -398,9 +405,9 @@ impl Command {
             | SQLCompile { io_args, .. }
             | SQLAnchor { io_args, .. }
             | SQLPreprocess(io_args)
-            | Debug(io_args)
-            | Annotate(io_args)
-            | Eval(io_args) => io_args.output.to_owned(),
+            | Debug(DebugCommand::Semantics(io_args))
+            | Debug(DebugCommand::Annotate(io_args))
+            | Debug(DebugCommand::Eval(io_args)) => io_args.output.to_owned(),
             _ => unreachable!(),
         };
         output.write_all(data)
@@ -619,7 +626,7 @@ mod tests {
     #[test]
     fn layouts() {
         let output = Command::execute(
-            &Command::Annotate(IoArgs::default()),
+            &Command::Debug(DebugCommand::Annotate(IoArgs::default())),
             &mut r#"
 from initial_table
 select {f = first_name, l = last_name, gender}
