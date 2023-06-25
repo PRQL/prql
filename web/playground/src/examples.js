f855f5d3f6c3f71ea1814b0eec1f3b52efd11681
Rich Brown <richb.hanover@gmail.com>
2023-06-25 16:40:52 -0400

    Tweak up PRQL example

--- web/playground/src/examples.js
+++ web/playground/src/examples.js
@@ -1,31 +1,32 @@
 const examples = {
   "introduction.prql": [
     "sql",
-    `from invoices                        # PRQL query begins with a table
-derive {                             # "derive" adds columns
+    `from invoices                        # A PRQL query begins with a table
+                                     # Subsequent lines "transform" (modify) it
+derive {                             # "derive" adds columns to the result
   transaction_fee = 0.8,             # "=" sets a column name
-  income = total - transaction_fees  # Calculations can use other column names
-}                                    # "#" starts a comment - even at start of line
-filter income > 1                    # "filter" replaces both of SQL's WHERE & HAVING
-filter invoice_date >= @1970-01-16   # Clear date syntax
+  income = total - transaction_fee   # Calculations can use other column names
+}
+# starts a comment; commenting out a line leaves a valid query
+filter income > 5                    # "filter" replaces both of SQL's WHERE & HAVING
+filter invoice_date >= @2010-01-16   # Clear date syntax
 group customer_id (                  # "group" performs the pipeline in (...) on each group
   aggregate {                        # "aggregate" reduces each group to a single row
-    average total,                   # get the average of the total for each customer_id
-    sum_income = sum income,         # ... and the total of income
-    ct = count customer_id,          # ... and the count of items
+    sum_income = sum income,         # ... using SQL SUM(), COUNT(), etc. functions
+    ct = count customer_id,          # 
   }
 )
 join c=customers (==customer_id)     # join on "customer_id" from both tables
 derive name = f"{c.last_name}, {c.first_name}" # F-strings like Python
 derive db_version = s"version()"     # S-string offers escape hatch to SQL
-select {                             # Select certain columns
+select {                             # "select" takes only the named columns
   c.customer_id, name, sum_income, ct, db_version,
 }                                    # trailing comma ignored
 sort {-sum_income}                   # "sort" sorts the result; "-" is decreasing order
 take 1..10                           # Limit to a range - could also be "take 10"
 #
-# "output.sql" tab at right shows the SQL generated from this PRQL query
-# "output.arrow" tab shows the result of the query
+# The "output.sql" tab at right shows the SQL generated from this PRQL query
+# The "output.arrow" tab shows the result of the query
 `,
   ],
 
