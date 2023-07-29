e903fe0d542cf58a3c75d5b2d7cab73d71371e25
eitsupi <ts1s1andn@gmail.com>
2023-07-29 09:42:46 +0000

    ci: use prql-compiler's version

--- .github/set_version.sh
+++ .github/set_version.sh
@@ -4,4 +4,5 @@
 # since the caches seem to accumulate cruft over time;
 # ref https://github.com/PRQL/prql/pull/2407
 
-echo "version=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')" >>"$GITHUB_ENV"
+version=$(cargo metadata --format-version=1 --no-deps | jq --raw-output '.packages[] | select(.name == "prql-compiler") | .version')
+echo "version=${version}" >>"$GITHUB_ENV"
