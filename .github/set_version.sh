0ebecb6861e2364454ee708ccca3b24357c76b46
eitsupi <ts1s1andn@gmail.com>
2023-07-29 08:44:57 +0000

    ci: use cargo metadata and jq to extract the version number

--- .github/set_version.sh
+++ .github/set_version.sh
@@ -2,4 +2,4 @@
 # since the caches seem to accumulate cruft over time;
 # ref https://github.com/PRQL/prql/pull/2407
 
-grep '^version =' Cargo.toml | tr -d ' "' >> $GITHUB_ENV
+echo "version=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')" >>"$GITHUB_ENV"
