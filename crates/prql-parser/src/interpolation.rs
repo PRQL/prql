7a59f1951c6c9259775000a47d6205f8b37bd893
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-08-06 11:21:03 -0700

    fix: s-strings within double brackets (#3265)

--- crates/prql-parser/src/interpolation.rs
+++ crates/prql-parser/src/interpolation.rs
@@ -59,13 +59,7 @@ fn parse_interpolate() {
     @r###"
     [
         String(
-            "print('",
-        ),
-        String(
-            "{hello}",
-        ),
-        String(
-            "')",
+            "print('{hello}')",
         ),
     ]
     "###);
@@ -75,31 +69,35 @@ fn parse_interpolate() {
     @r###"
     [
         String(
-            "concat('",
-        ),
-        String(
-            "{', a, '}",
-        ),
-        String(
-            "')",
+            "concat('{', a, '}')",
         ),
     ]
     "###);
 
-    // This is incorrect — it should return an inner expression, of the `a`, and
-    // convert the `{{` and `}}` to `{` and `}` respectively.
     assert_debug_snapshot!(
         parse("concat('{{', {a}, '}}')".to_string(), span_base).unwrap(), 
     @r###"
     [
         String(
-            "concat('",
+            "concat('{', ",
         ),
+        Expr {
+            expr: Expr {
+                kind: Ident(
+                    Ident {
+                        path: [],
+                        name: "a",
+                    },
+                ),
+                span: Some(
+                    0:13-16,
+                ),
+                alias: None,
+            },
+            format: None,
+        },
         String(
-            "{', {a}, '}",
-        ),
-        String(
-            "')",
+            ", '}')",
         ),
     ]
     "###);
@@ -123,19 +121,16 @@ fn parser(span_base: ParserSpan) -> impl Parser<char, Vec<InterpolateItem>, Erro
             InterpolateItem::Expr { expr, format }
         });
 
-    let escape = (just("{{").to('{'))
-        .chain(just("}}").not().repeated())
-        .chain(just("}}").to('}'))
-        .collect::<String>()
-        .map(InterpolateItem::String);
-
-    let string = none_of('{')
+    // Convert double braces to single braces, and fail on any single braces.
+    let string = (just("{{").to('{'))
+        .or(just("}}").to('}'))
+        .or(none_of("{}"))
         .repeated()
         .at_least(1)
         .collect::<String>()
         .map(InterpolateItem::String);
 
-    escape.or(expr).or(string).repeated().then_ignore(end())
+    expr.or(string).repeated().then_ignore(end())
 }
 
 fn offset_span(base: ParserSpan, range: std::ops::Range<usize>) -> ParserSpan {
