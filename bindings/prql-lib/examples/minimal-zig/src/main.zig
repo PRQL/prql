1a2508b1f07c885c5a4f3f950e3dcba802a49c7f
Jonathan <vanillajonathan@users.noreply.github.com>
2023-08-22 16:32:17 +0200

    Add files via upload

---
+++ bindings/prql-lib/examples/minimal-zig/src/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const prql = @cImport({
+    @cInclude("../c/libprql_lib.h");
+});
+
+pub fn main() !void {
+    var target = "sql.mssql".*;
+    // Setup PRQL compiler options
+    const options = prql.Options{
+        .format = false,
+        .signature_comment = false,
+        //.target = &target,
+        .target = &target
+    };
+
+    // Compile the PRQL query
+    const prql_query = "from albums | select {album_id, title} | take 3";
+    const result = prql.compile(prql_query, &options);
+    defer prql.result_destroy(result);
+
+    std.debug.print("Compiled with {d} errors\n", .{result.messages_len});
+    std.debug.print("Output:\n\n{s}\n", .{result.output});
+}
