856f293bac2d9adde5ca666584f9b8c4b261e783
Hulxv <m7mdx37@gmail.com>
2023-09-18 07:52:09 +0300

    feat: Special function to return PRQL version

--- crates/prql-compiler/src/semantic/eval/mod.rs
+++ crates/prql-compiler/src/semantic/eval/mod.rs
@@ -3,10 +3,10 @@ use std::iter::zip;
 use anyhow::Result;
 use itertools::Itertools;
 
+use super::ast_expand;
 use crate::error::{Error, Span, WithErrorInfo};
 use crate::ir::pl::{Expr, ExprKind, Func, FuncParam, Ident, Literal, PlFold};
-
-use super::ast_expand;
+use crate::COMPILER_VERSION;
 
 pub fn eval(expr: prql_ast::expr::Expr) -> Result<Expr> {
     let expr = ast_expand::expand_expr(expr)?;
@@ -277,6 +277,11 @@ impl Evaluator {
                 ExprKind::Array(array)
             }
 
+            "std.prql_version" => {
+                let ver = COMPILER_VERSION.to_string();
+                ExprKind::Literal(String(ver))
+            }
+
             _ => {
                 return Err(Error::new_simple(format!("unknown function {func_name}"))
                     .with_span(span)
@@ -399,6 +404,7 @@ fn std_module() -> Expr {
             new_func("columnar", &["closure", "relation"]),
             new_func("sum", &["x"]),
             new_func("lag", &["x"]),
+            new_func("prql_version", &[]),
         ]
         .to_vec(),
     ))
@@ -536,6 +542,21 @@ mod test {
             std.columnar {g = std.lag b}
         ").unwrap(),
             @"[{g = null}, {g = 4}, {g = 5}]"
+        );
+    }
+
+    #[test]
+    fn prql_version() {
+        println!("{:?}", eval("std.prql_version"));
+        println!(
+            "{:?}",
+            crate::compile(
+                r"
+            from updates
+            filter version = prql_version
+        ",
+                &crate::Options::default()
+            )
         );
     }
 }
