fd47bd822c0996ae89b49ef1b8ad5aa9bbe00ea6
Jonathan <vanillajonathan@users.noreply.github.com>
2023-09-23 01:52:15 +0200

    Add highlight.js

---
+++ grammars/prql-lezer/src/highlight.js
@@ -0,0 +1,17 @@
+import {styleTags, tags as t} from "@lezer/highlight"
+
+export const prqlHighlight = styleTags({
+  "let": t.definitionKeyword,
+  "case": t.controlKeyword,
+  "in": t.operatorKeyword,
+  Comment: t.lineComment,
+  Number: t.number,
+  String: t.string,
+  F_string: t.special(t.string),
+  R_string: t.special(t.string),
+  S_string: t.special(t.string),
+  "( )": t.paren,
+  "[ ]": t.squareBracket,
+  "{ }": t.brace,
+  "| ,": t.separator
+})
