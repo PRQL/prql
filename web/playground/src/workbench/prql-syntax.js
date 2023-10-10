55e82fefe57e134773882e2e945981cc6ef65126
Jonathan <vanillajonathan@users.noreply.github.com>
2023-10-10 22:57:14 +0200

    feat(Monarch grammar): Add token postfix and data types

--- web/playground/src/workbench/prql-syntax.js
+++ web/playground/src/workbench/prql-syntax.js
@@ -20,9 +20,23 @@ const LITERALS = ["null", "true", "false"];
 const def = {
   // Set defaultToken to invalid to see what you do not tokenize yet
   // defaultToken: 'invalid',
+  tokenPostfix: '.prql',
 
   keywords: [...TRANSFORMS, ...BUILTIN_FUNCTIONS, ...KEYWORDS, ...LITERALS],
 
+  typeKeywords: [
+    'bool',
+    'int8',
+    'int16',
+    'int32',
+    'int64',
+    'int128',
+    'int',
+    'float',
+    'text',
+    'set'
+  ],
+  
   operators: [
     "-",
     "*",
@@ -57,7 +71,15 @@ const def = {
       // identifiers and keywords
       [
         /[a-z_$][\w$]*/,
-        { cases: { "@keywords": "keyword", "@default": "identifier" } },
+        {
+          cases:
+          {
+            '@typeKeywords': 'keyword.type',
+            "@keywords": "keyword",
+            '@constants': 'keyword',
+            "@default": "identifier"
+          }
+        },
       ],
 
       // whitespace
