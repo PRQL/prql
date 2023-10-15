48eead0ad17b54c3625bf674a9f7f62fbdfe825d
Aljaž Mur Eržen <aljaz@edgedb.com>
2023-10-15 21:30:40 +0200

    fix: report syntax error from all files

--- crates/prql-compiler/src/parser.rs
+++ crates/prql-compiler/src/parser.rs
@@ -1,14 +1,13 @@
+use anyhow::Result;
 use std::collections::HashMap;
 
-use anyhow::Result;
-use prql_ast::stmt::Stmt;
-
 use crate::utils::IdGenerator;
-use crate::Errors;
-use crate::SourceTree;
+use crate::{Errors, SourceTree};
+use prql_ast::stmt::Stmt;
 
 pub fn parse(file_tree: &SourceTree<String>) -> Result<SourceTree<Vec<Stmt>>> {
     let mut res = SourceTree::default();
+    let mut errors = Vec::new();
 
     let ids: HashMap<_, _> = file_tree.source_ids.iter().map(|(a, b)| (b, a)).collect();
     let mut id_gen = IdGenerator::<usize>::new();
@@ -18,16 +17,18 @@ pub fn parse(file_tree: &SourceTree<String>) -> Result<SourceTree<Vec<Stmt>>> {
             .get(path)
             .map(|x| **x)
             .unwrap_or_else(|| id_gen.gen() as u16);
-        let stmts = parse_source(source, id)?;
 
-        res.sources.insert(path.clone(), stmts);
-        res.source_ids.insert(id, path.clone());
+        match prql_parser::parse_source(source, id) {
+            Ok(stmts) => {
+                res.sources.insert(path.clone(), stmts);
+                res.source_ids.insert(id, path.clone());
+            }
+            Err(errs) => errors.extend(errs),
+        }
     }
-    Ok(res)
-}
-
-fn parse_source(source: &str, source_id: u16) -> Result<Vec<prql_ast::stmt::Stmt>> {
-    let stmts = prql_parser::parse_source(source, source_id).map_err(Errors)?;
-
-    Ok(stmts)
+    if errors.is_empty() {
+        Ok(res)
+    } else {
+        Err(Errors(errors).into())
+    }
 }
