b9d82c3751904bcbcbe913ad1e614419ff0a850d
Cédric L. Charlier <seddryck@gmail.com>
2023-11-12 13:52:00 +0100

    refactor: improve function descriptions and exception messages

--- prqlc/bindings/dotnet/PrqlCompiler/PrqlCompiler.cs
+++ prqlc/bindings/dotnet/PrqlCompiler/PrqlCompiler.cs
@@ -18,9 +18,7 @@ namespace Prql.Compiler
         public static Result Compile(string prqlQuery)
         {
             if (string.IsNullOrEmpty(prqlQuery))
-            {
-                throw new ArgumentException(nameof(prqlQuery));
-            }
+                throw new ArgumentException("PRQL query cannot be null or empty.", nameof(prqlQuery));
 
             var options = new PrqlCompilerOptions();
 
@@ -34,19 +32,11 @@ namespace Prql.Compiler
         /// <param name="options">PRQL compiler options.</param>
         /// <returns>SQL query.</returns>
         /// <exception cref="ArgumentException"><paramref name="prqlQuery"/> is null or empty.</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="options"/> is <c>null</c>.</exception>
         /// <exception cref="FormatException"><paramref name="prqlQuery"/> cannot be compiled.</exception>
         public static Result Compile(string prqlQuery, PrqlCompilerOptions options)
         {
             if (string.IsNullOrEmpty(prqlQuery))
-            {
-                throw new ArgumentException(nameof(prqlQuery));
-            }
-
-            if (options is null)
-            {
-                throw new ArgumentException(nameof(options));
-            }
+                throw new ArgumentException("PRQL query cannot be null or empty.", nameof(prqlQuery));
 
             var nativeOptions = new NativePrqlCompilerOptions(options);
             var nativeResult = CompileExtern(prqlQuery, ref nativeOptions);
@@ -66,9 +56,7 @@ namespace Prql.Compiler
         public static Result PrqlToPl(string prqlQuery)
         {
             if (string.IsNullOrEmpty(prqlQuery))
-            {
-                throw new ArgumentException(nameof(prqlQuery));
-            }
+                throw new ArgumentException("PRQL query cannot be null or empty.", nameof(prqlQuery));
 
             var nativeResult = PrqlToPlExtern(prqlQuery);
             var result = new Result(nativeResult);
@@ -79,7 +67,7 @@ namespace Prql.Compiler
         /// <summary>
         /// Finds variable references, validates functions calls, determines frames and converts PL to RQ.
         /// </summary>
-        /// <param name="plJson">A PRQL query.</param>
+        /// <param name="plJson">A PL string in JSON format.</param>
         /// <returns>JSON.</returns>
         /// <exception cref="ArgumentException"><paramref name="plJson"/> is null or empty.</exception>
         /// <exception cref="FormatException"><paramref name="plJson"/> cannot be compiled.</exception>
@@ -87,9 +75,7 @@ namespace Prql.Compiler
         public static Result PlToRq(string plJson)
         {
             if (string.IsNullOrEmpty(plJson))
-            {
-                throw new ArgumentException(nameof(plJson));
-            }
+                throw new ArgumentException("PL json cannot be null or empty.", nameof(plJson));
 
             var nativeResult = PlToRqExtern(plJson);
             var result = new Result(nativeResult);
@@ -110,14 +96,7 @@ namespace Prql.Compiler
         public static Result RqToSql(string rqJson, PrqlCompilerOptions options)
         {
             if (string.IsNullOrEmpty(rqJson))
-            {
-                throw new ArgumentException(nameof(rqJson));
-            }
-
-            if (options is null)
-            {
-                throw new ArgumentException(nameof(options));
-            }
+                throw new ArgumentException("PL json cannot be null or empty.", nameof(rqJson));
 
             var nativeOptions = new NativePrqlCompilerOptions(options);
             var nativeResult = RqToSqlExtern(rqJson, ref nativeOptions);
