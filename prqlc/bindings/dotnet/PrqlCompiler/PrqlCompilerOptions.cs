2f85dc07b40984fc19b57f407ed6033fd24d492b
Cédric L. Charlier <seddryck@gmail.com>
2023-11-12 13:56:39 +0100

    refactor: make PrqlCompilerOptions immutable

--- prqlc/bindings/dotnet/PrqlCompiler/PrqlCompilerOptions.cs
+++ prqlc/bindings/dotnet/PrqlCompiler/PrqlCompilerOptions.cs
@@ -6,24 +6,25 @@ namespace Prql.Compiler
     /// Compilation options for SQL backend of the compiler.
     /// </summary>
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-    public class PrqlCompilerOptions
-    {
+    public record struct PrqlCompilerOptions
+    (
         /// <summary>
         /// Pass generated SQL string trough a formatter that splits it into
         /// multiple lines and prettifies indentation and spacing.
         /// </summary>
         /// <remarks>Defaults to <c>true</c>.</remarks>
-        public bool Format { get; set; } = true;
+        bool Format = true,
 
         /// <summary>
         /// Target and dialect to compile to.
         /// </summary>
-        public string Target { get; set; }
+        string Target = "",
 
         /// <summary>
         /// Emits the compiler signature as a comment after generated SQL.
         /// </summary>
         /// <remarks>Defaults to <c>true</c>.</remarks>
-        public bool SignatureComment { get; set; } = true;
-    }
+        bool SignatureComment = true
+    )
+    { }
 }
