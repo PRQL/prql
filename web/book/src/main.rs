79945936daeb8f113c543f5c884195f07bf42c80
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-11-17 09:42:24 -0800

    tweak: Remove old comment (#3816)

--- web/book/src/main.rs
+++ web/book/src/main.rs
@@ -1,7 +1,3 @@
-// All copied from `mdbook_preprocessor_boilerplate` apart from the function
-// which does the replacement.
-// This file is licensed under GPL-3.0 then. We don't link against it from PRQL.
-
 // We don't need to run this with wasm, and the features that `mdbook` uses of
 // `clap`'s don't support wasm.
 #[cfg(not(target_family = "wasm"))]
