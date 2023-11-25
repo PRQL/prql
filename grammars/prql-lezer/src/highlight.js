e567412bcef3f54338687b6e07c5b7ba1aa172d4
Jonathan <vanillajonathan@users.noreply.github.com>
2023-11-25 22:48:09 +0100

    feat(Lezer grammar): Highlight properties

--- grammars/prql-lezer/src/highlight.js
+++ grammars/prql-lezer/src/highlight.js
@@ -12,6 +12,7 @@ export const prqlHighlight = styleTags({
   Boolean: t.bool,
   Integer: t.integer,
   Float: t.float,
+  PropertyName: t.propertyName,
   TypeTerm: t.typeName,
   Escape: t.escape,
   String: t.string,
