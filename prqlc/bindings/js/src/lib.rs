3064c502e38bad36f0b246d21091b3b2c00f2b7d
John Jacquay <john@bioteam.net>
2023-12-18 16:02:59 -0700

    Implemented pl_to_prql for js bindings

--- prqlc/bindings/js/src/lib.rs
+++ prqlc/bindings/js/src/lib.rs
@@ -23,6 +23,15 @@ pub fn prql_to_pl(prql_query: &str) -> Option<String> {
 }
 
 #[wasm_bindgen]
+pub fn pl_to_prql(pl_json: &str) -> Option<String> {
+    return_or_throw(
+        Ok(pl_json)
+            .and_then(prql_compiler::json::to_pl)
+            .and_then(prql_compiler::pl_to_prql)
+    )
+}
+
+#[wasm_bindgen]
 pub fn pl_to_rq(pl_json: &str) -> Option<String> {
     return_or_throw(
         Ok(pl_json)
