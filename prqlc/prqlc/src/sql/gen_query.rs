19d81c725137d49ae5d2df522a469e9e34c70f3a
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2024-04-26 16:38:52 -0700

    Update prqlc/prqlc/src/sql/gen_query.rs

--- prqlc/prqlc/src/sql/gen_query.rs
+++ prqlc/prqlc/src/sql/gen_query.rs
@@ -447,7 +447,7 @@ fn translate_relation_literal(data: RelationLiteral, ctx: &Context) -> Result<sq
         return Err(
             Error::new_simple("No rows provided for `from_text`".to_string()).push_hint(
                 "add a newline, then a row of data following the column. If using \
-                the json format, ensure `data` isnt empty",
+                the json format, ensure `data` isn't empty",
             ),
         );
     }
