366261cc27ff40fdf2b7909a30c9108e57f41338
Maximilian Roos <m@maxroos.com>
2024-05-19 18:20:35 -0700

    test: Add test & example for `prepare_stream`

--- prqlc/prqlc-parser/src/lib.rs
+++ prqlc/prqlc-parser/src/lib.rs
@@ -147,6 +147,33 @@ fn prepare_stream(
     Stream::from_iter(eoi, tokens)
 }
 
+#[test]
+fn test_prepare_stream() {
+    use insta::assert_yaml_snapshot;
+
+    let input = "from artists | filter name == 'John'";
+    let tokens = lex_source(input).unwrap();
+    let mut stream = prepare_stream(tokens.0.into_iter(), input, 0);
+    assert_yaml_snapshot!(stream.fetch_tokens().collect::<Vec<(TokenKind, ParserSpan)>>(), @r###"
+    ---
+    - - Ident: from
+      - "0:0-4"
+    - - Ident: artists
+      - "0:5-12"
+    - - Control: "|"
+      - "0:13-14"
+    - - Ident: filter
+      - "0:15-21"
+    - - Ident: name
+      - "0:22-26"
+    - - Eq
+      - "0:27-29"
+    - - Literal:
+          String: John
+      - "0:30-36"
+    "###);
+}
+
 fn convert_lexer_error(source: &str, e: chumsky::error::Cheap<char>, source_id: u16) -> Error {
     // We want to slice based on the chars, not the bytes, so can't just index
     // into the str.
