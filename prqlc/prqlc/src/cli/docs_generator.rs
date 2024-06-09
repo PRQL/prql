12110745ab576b52a453280796b859232d22fff4
Maximilian Roos <m@maxroos.com>
2024-06-08 17:56:44 -0700

    chore: Bump version to 0.12.2

--- prqlc/prqlc/src/cli/docs_generator.rs
+++ prqlc/prqlc/src/cli/docs_generator.rs
@@ -404,7 +404,7 @@ mod tests {
 
 
 
-        Generated with [prqlc](https://prql-lang.org/) 0.12.1.
+        Generated with [prqlc](https://prql-lang.org/) 0.12.2.
 
         ----- stderr -----
         "###);
