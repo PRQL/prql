2bb27db76aa0c16d37d576f99f3ad9fb203447ed
Maximilian Roos <m@maxroos.com>
2024-06-19 18:31:51 -0700

    add comment re exports

--- prqlc/prqlc-parser/src/parser/pr/mod.rs
+++ prqlc/prqlc-parser/src/parser/pr/mod.rs
@@ -1,19 +1,20 @@
-//this exports everything within as if it was at the root.
-//eg: PR::expr::Expr becomes PR::Expr
+// TODO: exporting `foo::*` and exporting the module `foo` seems prone to
+// confusion and IIUC serves no purpose — IIUC it guarantees there are two ways
+// of accessing the same variable
+
+pub mod expr;
+pub mod ident;
+pub mod ops;
+pub mod stmt;
+pub mod types;
+
 pub use expr::*;
 pub use ident::*;
 pub use ops::*;
 pub use stmt::*;
 pub use types::*;
 
-//re-export Literal from LR, since it's encapsulated in TyKind
-
-pub mod expr;
-pub mod stmt;
-
-pub mod ident;
-pub mod ops;
-pub mod types;
+// re-export Literal from LR, since it's encapsulated in TyKind
 
 pub use crate::lexer::lr::Literal;
 pub use crate::lexer::lr::ValueAndUnit;
