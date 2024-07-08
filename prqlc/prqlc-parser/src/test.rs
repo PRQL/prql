577924a058fa22b8a99046832b2d796168d9d8f7
Maximilian Roos <m@maxroos.com>
2024-07-07 21:31:15 -0700

    Revert "fix: Enforce a line break between annotations"
    This reverts commit e5a4f268dcccd6835e278629bf53c73178732adc.

--- prqlc/prqlc-parser/src/test.rs
+++ prqlc/prqlc-parser/src/test.rs
@@ -2871,14 +2871,6 @@ fn test_annotation() {
         "#,
     )
     .unwrap();
-
-    parse_single(
-        r#"
-        @{binding_strength=1}@{binding_strength=2}
-        let add = a b -> a + b
-        "#,
-    )
-    .unwrap_err();
 }
 
 #[test]
