ab7905c523cecd48ca2b86dbf937714c7f1098e1
Maximilian Roos <m@maxroos.com>
2024-07-07 21:30:59 -0700

    Revert ""
    This reverts commit 8b9d45feb773e6388e9c5c585f0dfd819249502a.

--- prqlc/prqlc-parser/src/parser/stmt.rs
+++ prqlc/prqlc-parser/src/parser/stmt.rs
@@ -28,7 +28,7 @@ fn module_contents() -> impl Parser<TokenKind, Vec<Stmt>, Error = PError> {
 
         let annotation = just(TokenKind::Annotate)
             .ignore_then(expr())
-            .then_ignore(new_line().repeated().at_least(1))
+            .then_ignore(new_line().repeated())
             .map(|expr| Annotation {
                 expr: Box::new(expr),
                 aesthetics_before: Vec::new(),
