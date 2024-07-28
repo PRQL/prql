464eda77b55ac7469aadb7387cf913f375150303
Maximilian Roos <m@maxroos.com>
2024-07-27 17:04:39 -0700

    refine: Add doc comment to `pr` module

--- prqlc/prqlc-parser/src/parser/pr/mod.rs
+++ prqlc/prqlc-parser/src/parser/pr/mod.rs
@@ -1,8 +1,6 @@
-mod expr;
-mod ident;
-mod ops;
-mod stmt;
-mod types;
+//! PR, or "Parser Representation" is an AST representation of parsed PRQL. It
+//! takes LR tokens and converts them into a more structured form which
+//! understands expressions, such as tuples & functions.
 
 pub use expr::*;
 pub use ident::*;
@@ -13,3 +11,9 @@ pub use types::*;
 // re-export Literal from LR, since it's encapsulated in TyKind
 pub use crate::lexer::lr::Literal;
 pub use crate::span::Span;
+
+mod expr;
+mod ident;
+mod ops;
+mod stmt;
+mod types;
